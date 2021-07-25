79908d01026cec0ee7dc788218c1ea2d904d7a4e
Rey Matthew <51887031+mreymatthewe@users.noreply.github.com>
2021-07-26 06:11:56 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Fresh.ly.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Fresh.ly.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Stock { get; set; }
+        public int MyProperty { get; set; }
+        public virtual Supplier Supplier { get; set; }
+        public int? SupplierID { get; set; }
+
+    }
+
+    public class Supplier
+    {
+        public int SupplierID { get; set; }
+        public string Name { get; set; }
+    }
+}
